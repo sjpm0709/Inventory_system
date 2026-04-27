0fb9381468e5204d6cf5c0a819e556a288d3ffc9
sjpm0709 <satishjpmishra@gmail.com>
2026-04-27 16:12:38 +0530

    Update and rename production_ui.py to production.py

---
+++ ui/production.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from services.product_service import get_products
+from services.production_service import produce
+
+def show_production():
+    st.header("Production")
+
+    products = get_products()
+    product_dict = {sku: id for id, sku in products}
+
+    if not product_dict:
+        st.warning("No products found. Add products first.")
+        return
+
+    selected_product = st.selectbox("Select SKU", list(product_dict.keys()))
+    qty = st.number_input("Quantity to Produce", min_value=1)
+
+    if st.button("Produce"):
+        try:
+            produce(product_dict[selected_product], selected_product, qty)
+            st.success("Production Completed")
+        except Exception as e:
+            st.error(str(e))
